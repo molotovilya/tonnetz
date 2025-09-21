736321b4506ed7e774e0414c06c4b90e31dfb246
Ilya Molotov <Molotovilyasrg@gmail.com>
2025-09-21 18:34:14 +0700

    добавил дополнительные октавы qwerty

--- js/index.js
+++ js/index.js
@@ -81,12 +81,11 @@ proto = new Vue({
         
         // QWERTY keyboard bindings - More intuitive layout
         ascii: JZZ.input.ASCII({
-            // Upper row: White keys (a whole tone scale)
-            A: 'C3', S: 'D3', D: 'E3', F: 'F3', G: 'G3', H: 'A3', J: 'B3', K: 'C4', L: 'D4', 
-            // Lower row: Black keys (the sharps/flats)
-            W: 'C#3', E: 'D#3', T: 'F#3', Y: 'G#3', U: 'A#3', O: 'C#4', P: 'D#4'
+            '2': 'C#4', '3': 'D#4', '5': 'F#4', '6': 'G#4', '7': 'A#4','9': 'C#5', '0': 'D#5', '=': 'F#5', '-': 'G#5',
+            Q: 'C4', W: 'D4', E: 'E4', R: 'F4', T: 'G4', Y: 'A4', U: 'B4',I: 'C5', O: 'D5', P: 'E5', '[': 'F5', ']': 'G5',
+            S: 'C#3', D: 'D#3', G: 'F#3', H: 'G#3', J: 'A#3', L: 'C#4',  ';': 'D#4',
+            Z: 'C3', X: 'D3', C: 'E3', V: 'F3', B: 'G3', N: 'A3', M: 'B3',',': 'C4', '.': 'D4', '/': 'E4'
         }),
-
 
 
         // Should trajectory drawing be active?
